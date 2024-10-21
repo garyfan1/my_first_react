d35a1e0a5e5ef24a7e45b69eed21a89bd939949e
Gary Fan <garyfan12@gmail.com>
2024-10-21 02:53:38 -0700

    delete css files, start implementing headers and navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { AppShell } from "@mantine/core";
+import { Burger } from "@mantine/core";
 
-import '@mantine/core/styles.css';
+import Content from "./content";
 
-import { MantineProvider, createTheme } from '@mantine/core';
+import "@mantine/core/styles.css"; // needed at the root of App to use Mantine
+
+import { MantineProvider, createTheme } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
 
 function App() {
-
   const theme = createTheme({
     /** Put your mantine theme override here */
   });
 
-  const [count, setCount] = useState(0)
+  const [opened, { toggle }] = useDisclosure();
 
   return (
     <MantineProvider theme={theme}>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <AppShell
+        header={{ height: 60 }}
+        navbar={{
+          width: 250,
+          breakpoint: "sm",
+        }}
+        padding="md"
+      >
+        <AppShell.Header>this is the header</AppShell.Header>
+
+        <AppShell.Navbar p="md">
+          <Burger opened={opened} onClick={toggle} />
+        </AppShell.Navbar>
+
+        <AppShell.Main>
+          <Content />
+        </AppShell.Main>
+      </AppShell>
     </MantineProvider>
-  )
+  );
 }
 
-export default App
+export default App;
