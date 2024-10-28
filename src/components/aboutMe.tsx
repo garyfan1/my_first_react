077e4b7b9442f9dc112352f87556bb19f39ff68f
Gary Fan <garyfan12@gmail.com>
2024-10-27 23:55:54 -0700

    add content for projects

--- src/components/aboutMe.tsx
+++ src/components/aboutMe.tsx
@@ -1,7 +1,34 @@
+import { Text, Title } from "@mantine/core";
+
 function AboutMe() {
   return (
     <>
-      <h1>About Me</h1>
+      <Title>About Me</Title>
+      <Text>
+        I initially planned to major in civil engineering, but while taking an
+        elective course in C++, I developed a strong interest in coding and
+        decided to switch my focus to computer science.
+      </Text>
+      <Text>
+        During the pandemic, I took a semester off to work with a startup in
+        Taiwan, where I deepened my skills in AWS by developing Lambda functions
+        and EventBridge events.
+      </Text>
+      <Text>
+        After returning to complete my degree at the University of Victoria, I
+        gained hands-on experience with web technologies like Vue.js and Angular
+        through co-op placements with Schneider Electric and the Government of
+        BC.
+      </Text>
+      <Text>
+        Upon graduating, I rejoined the startup in Taiwan as a backend
+        developer, focusing on API development with Django in a fast-paced
+        environment.
+      </Text>
+      <Text>
+        Fun Fact about me: I eat pizza backwards (i.e., starting from the crust)
+        AND with chopsticks.
+      </Text>
     </>
   );
 }
