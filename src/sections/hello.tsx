c65700b34767a8dbdf3490592bce8fd318e2f31a
Gary Fan <garyfan12@gmail.com>
2024-11-20 03:34:37 -0800

    make hello setion responsive

--- src/sections/hello.tsx
+++ src/sections/hello.tsx
@@ -1,31 +1,50 @@
-import { ActionIcon, Container, Flex, Group, Title } from "@mantine/core";
+import {
+  ActionIcon,
+  Grid,
+  Group,
+  Title,
+  TitleOrder,
+  useMatches,
+} from "@mantine/core";
 import {
   IconBrandGithub,
   IconBrandLinkedin,
   IconMail,
 } from "@tabler/icons-react";
 import info from "../assets/info.json";
+
 const Hello = () => {
+  const titleOrder = useMatches<TitleOrder>({
+    base: 2,
+    sm: 1,
+  });
+
+  const myLinksOrder = useMatches<TitleOrder>({
+    base: 4,
+    sm: 3,
+  });
+
   return (
     <>
-      <Flex justify="flex-start">
-        <Container w="70%" pl={12} ml={0}>
-          <Title mb={40} order={1}>
+      <Grid>
+        <Grid.Col ml={12} span={{ base: 11, md: 9 }}>
+          <Title mb={40} order={titleOrder}>
             Hello!
           </Title>
-          <Title mb={40} order={1}>
+          <Title mb={40} order={titleOrder}>
             I'm Gary Fan
           </Title>
-          <Title mb={40} order={1}>
+          <Title mb={40} order={titleOrder}>
             A software engineer specializing in web development and backend
             solutions, focused on building scalable and efficient applications
             with technologies like Django, Vue.js, Angular, and AWS.
           </Title>
-        </Container>
-      </Flex>
+        </Grid.Col>
+        <Grid.Col span={"auto"}></Grid.Col>
+      </Grid>
 
-      <Group pl={12} ml={0} gap="sm">
-        <Title order={3}>My Links</Title>
+      <Group ml={12} gap="sm">
+        <Title order={myLinksOrder}>My Links</Title>
         <ActionIcon
           variant="transparent"
           component="a"
