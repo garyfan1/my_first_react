1664d63476507be7a79902b9ea8c656da98ae061
Gary Fan <garyfan12@gmail.com>
2025-01-19 17:55:58 -0800

    add typing effect on hello

--- src/sections/hello.tsx
+++ src/sections/hello.tsx
@@ -12,6 +12,8 @@ import {
   IconMail,
 } from "@tabler/icons-react";
 import info from "../assets/info.json";
+import classes from "../components/hello.module.css";
+import { useEffect, useState } from "react";
 
 const Hello = () => {
   const titleOrder = useMatches<TitleOrder>({
@@ -24,24 +26,37 @@ const Hello = () => {
     sm: 3,
   });
 
+  const title = `Hello!\n\nI'm Gary Fan\n\nA software engineer specializing in web development and backend solutions, focused on building scalable and efficient applications with technologies like Django, Vue.js, Angular, and AWS.`;
+
+  const [curTitle, setTitle] = useState("");
+
+  const typeTitle = () => {
+    let temp = "";
+    let i = 0;
+    title.split("").forEach((c) => {
+      setTimeout(() => {
+        temp = temp + c;
+        setTitle(temp);
+      }, 1000 + 30 * i);
+      i++;
+    });
+  };
+
+  useEffect(() => {
+    typeTitle();
+  }, []);
+
   return (
     <>
-      <Grid>
+      <Grid mih={{ base: 500, sm: 400 }}>
         <Grid.Col
           mt={{ base: 30, sm: 0 }}
           ml={{ base: 30, sm: 12 }}
           span={{ base: 11, md: 9 }}
         >
-          <Title mb={40} order={titleOrder}>
-            Hello!
-          </Title>
-          <Title mb={40} order={titleOrder}>
-            I'm Gary Fan
-          </Title>
-          <Title mb={40} order={titleOrder}>
-            A software engineer specializing in web development and backend
-            solutions, focused on building scalable and efficient applications
-            with technologies like Django, Vue.js, Angular, and AWS.
+          <Title mb={40} order={titleOrder} style={{ whiteSpace: "pre-wrap" }}>
+            {curTitle}
+            <span className={classes.blink}>&nbsp;</span>
           </Title>
         </Grid.Col>
         <Grid.Col span={"auto"}></Grid.Col>
